64b4f00b4d75b20a7d05ed50dad339a7316e34ed
PythonicNerd <neilaylor@hotmail.com>
2017-11-13 14:36:58 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from googletrans import Translator
+import random
+
+t = Translator()
+#languages = ['af','ak','sq','am','ar','hy','az','eu','be']
+
+languages = ['af', 'ach', 'ak', 'am', 'ar', 'az', 'be', 'bem', 'bg', 'bh', 'bn', 'br', 'bs', 'ca', 'chr', 'ckb','co', 'crs', 'cs', 'cy', 'da', 'de', 'ee', 'el', 'en', 'eo', 'es', 'es-419', 'et', 'eu', 'fa', 'fi','fo', 'fr', 'fy', 'ga', 'gaa', 'gd', 'gl', 'gn', 'gu', 'ha', 'haw', 'hi', 'hr', 'ht', 'hu', 'hy', 'ia']
+
+"""
+id, ig, is, it, iw, ja, jw, ka, kg, kk, km, kn, ko, kri, ku, ky, la,
+lg, ln, lo, loz, lt, lua, lv, mfe, mg, mi, mk, ml, mn, mo, mr, ms, mt,
+ne, nl, nn, no, nso, ny, nyn, oc, om, or, pa, pcm, pl, ps, pt-BR,
+pt-PT, qu, rm, rn, ro, ru, rw, sd, sh, si, sk, sl, sn, so, sq, sr,
+sr-ME, st, su, sv, sw, ta, te, tg, th, ti, tk, tl, tn, to, tr, tt,
+tum, tw, ug, uk, ur, uz, vi, wo, xh, xx-bork, xx-elmer, xx-hacker,
+xx-klingon, xx-pirate, yi, yo, zh-CN, zh-TW, zu]
+"""
+
+
+text = input("Enter a sentence: \n> ")
+copy = t.translate(text, dest='ja')
+
+
+for i in range(30d):
+    print("On recursion: " + str(i))
+    try:
+        copy = t.translate(copy.text, dest=random.choice(languages))
+        print(copy.text)
+    except:
+        pass
+
+
+for i in range(10):
+    print('\n')
+print(t.translate(copy.text, dest='en').text)
