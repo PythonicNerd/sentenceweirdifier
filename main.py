3249747414782fb921d6a52b8ee20e1d34c2e74e
PythonicNerd <neilaylor@hotmail.com>
2017-11-14 14:36:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 t = Translator()
 #languages = ['af','ak','sq','am','ar','hy','az','eu','be']
 
-languages = ['af', 'ach', 'ak', 'am', 'ar', 'az', 'be', 'bem', 'bg', 'bh', 'bn', 'br', 'bs', 'ca', 'chr', 'ckb','co', 'crs', 'cs', 'cy', 'da', 'de', 'ee', 'el', 'en', 'eo', 'es', 'es-419', 'et', 'eu', 'fa', 'fi','fo', 'fr', 'fy', 'ga', 'gaa', 'gd', 'gl', 'gn', 'gu', 'ha', 'haw', 'hi', 'hr', 'ht', 'hu', 'hy', 'ia']
+#languages = ['af', 'ach', 'ak', 'am', 'ar', 'az', 'be', 'bem', 'bg', 'bh', 'bn', 'br', 'bs', 'ca', 'chr', 'ckb','co', 'crs', 'cs', 'cy', 'da', 'de', 'ee', 'el', 'en', 'eo', 'es', 'es-419', 'et', 'eu', 'fa', 'fi','fo', 'fr', 'fy', 'ga', 'gaa', 'gd', 'gl', 'gn', 'gu', 'ha', 'haw', 'hi', 'hr', 'ht', 'hu', 'hy', 'ia']
 
 """
 id, ig, is, it, iw, ja, jw, ka, kg, kk, km, kn, ko, kri, ku, ky, la,
@@ -16,16 +16,23 @@ tum, tw, ug, uk, ur, uz, vi, wo, xh, xx-bork, xx-elmer, xx-hacker,
 xx-klingon, xx-pirate, yi, yo, zh-CN, zh-TW, zu]
 """
 
+languages  = ['japanese','afrikaans','arabic','chinese','irish','french','italian','zulu','swahili','turkish','korean','french','icelandic','hindi','greek','german','dutch','czech','russian','hawaiian','polish','thai','spanish','yiddish']
+
 
 text = input("Enter a sentence: \n> ")
-copy = t.translate(text, dest='ja')
+copy = t.translate(text, dest='english')
 
 
-for i in range(30d):
-    print("On recursion: " + str(i))
+for i in range(30):
     try:
+        print(str(i) + ')')
         copy = t.translate(copy.text, dest=random.choice(languages))
         print(copy.text)
+        print('Language: ' + str(copy.src))
+        print('In english: ' + t.translate(copy.text, dest='english').text)
+        print('\n')
+        print('---------------------------------------------------------------')
+
     except:
         pass
 
